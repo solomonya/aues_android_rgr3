f2c92e08c242e48bdd7eb84aa4f765f32baf5458
Khan Solomon <s.khan@globerce.capital>
2023-12-08 16:41:11 +0600

    init

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,70 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonRow,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
+import { ContactPayload, Contacts } from "@capacitor-community/contacts";
+import { useEffect, useState } from "react";
+import { callOutline, personOutline } from "ionicons/icons";
 
 const Tab1: React.FC = () => {
+  const [contacts, setContacts] = useState<ContactPayload[]>([]);
+  const getContacts = async () => {
+    const projection = {
+      name: true,
+      phones: true,
+      postalAddresses: true,
+    };
+
+    const result = await Contacts.getContacts({
+      projection,
+    });
+    setContacts(result.contacts);
+  };
+  useEffect(() => {
+    getContacts();
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Contacts List</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Contacts List</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        {contacts.map(({ name, phones }) => (
+          <IonCard>
+            <IonCardContent>
+              <IonRow>
+                <IonIcon icon={personOutline}></IonIcon>
+                <IonText>
+                  <p>{name?.display}</p>
+                </IonText>
+              </IonRow>
+              <IonRow>
+                <IonIcon icon={callOutline}></IonIcon>
+                <IonText>
+                  <p>{phones?.[0]?.number}</p>
+                </IonText>
+              </IonRow>
+            </IonCardContent>
+          </IonCard>
+        ))}
       </IonContent>
     </IonPage>
   );
